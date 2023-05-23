2dabb87d64993b19b6deab34a74a12b2d540a2b6
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-05-23 20:02:37 -0300

    Ajustado EstadoControle colocando o mapping de GET, POST e DELETE

--- Backend/src/main/java/com/castilho/backend/controle/EstadoControle.java
+++ Backend/src/main/java/com/castilho/backend/controle/EstadoControle.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,15 +22,20 @@ public class EstadoControle {
     @Autowired
     private EstadoServico estadoServico;
 
-    
+    // localhost:8080/estado/ - com verbo get
+    @GetMapping 
     public List<Estado> listarTodos(){
         return estadoServico.listarTodos();
     }
 
+    // localhost:8080/estado/ - com verbo post
+    @PostMapping
     public Estado salvar(@RequestBody Estado estado){
         return estadoServico.salvar(estado);
     }
 
+    // localhost:8080/estado/1 - com verbo delete
+    @DeleteMapping("/{id}")
     public void excluir(Long id){
         estadoServico.excluir(id);
     }
