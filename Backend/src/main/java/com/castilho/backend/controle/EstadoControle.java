8e948c959091e22e9cf34129a95874b0b25d89bb
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-05-23 20:29:36 -0300

    Colocado tratativa de exceção no controle do Estado

--- Backend/src/main/java/com/castilho/backend/controle/EstadoControle.java
+++ Backend/src/main/java/com/castilho/backend/controle/EstadoControle.java
@@ -1,11 +1,15 @@
 package com.castilho.backend.controle;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +40,12 @@ public class EstadoControle {
 
     // localhost:8080/estado/1 - com verbo delete
     @DeleteMapping("/{id}")
-    public void excluir(Long id){
-        estadoServico.excluir(id);
+    public ResponseEntity<Object> excluirEstado(@PathVariable Long id) {
+        try {
+            estadoServico.excluir(id);
+            return ResponseEntity.ok("Estado excluído com sucesso");
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
