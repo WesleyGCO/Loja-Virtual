532d09de93a5c13efa7f36b46c58749263fce036
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-05-23 21:44:11 -0300

    Criado entidade de Permissao

---
+++ Backend/src/main/java/com/castilho/backend/entidade/Permissao.java
@@ -0,0 +1,33 @@
+package com.castilho.backend.entidade;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "permissao")
+public class Permissao {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "data_criacao")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+}
