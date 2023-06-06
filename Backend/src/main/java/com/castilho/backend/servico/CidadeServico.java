554401dac123bb8567330585da01b5af2c3e5c6b
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-06-05 22:44:16 -0300

    Modificado os serviços de cidade e estado

--- Backend/src/main/java/com/castilho/backend/servico/CidadeServico.java
+++ Backend/src/main/java/com/castilho/backend/servico/CidadeServico.java
@@ -1,30 +1,73 @@
 package com.castilho.backend.servico;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.castilho.backend.repositorio.CidadeRepositorio;
+import com.castilho.backend.repositorio.EstadoRepositorio;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import jakarta.transaction.Transactional;
 
 import com.castilho.backend.entidade.Cidade;
+import com.castilho.backend.entidade.Estado;
 
 @Service
 public class CidadeServico {
     
     @Autowired
     private CidadeRepositorio cidadeRepositorio;
+    @Autowired
+    private EstadoRepositorio estadoRepositorio;
 
     public List<Cidade> listarTodos(){
         return cidadeRepositorio.findAll();
     }
 
-    public Cidade salvar(Cidade cidade){
-        return cidadeRepositorio.save(cidade);
+    public Cidade inserir(Cidade cidade){
+        cidade.setDataCriacao(new Date());
+        Cidade cidadeNova = cidadeRepositorio.saveAndFlush(cidade);
+        return cidadeNova;
+    }
+
+    public Cidade atualizar(Cidade cidade){
+        cidade.setDataAtualizacao(new Date());
+        Cidade cidadeAtualizada = cidadeRepositorio.saveAndFlush(cidade);
+        return cidadeAtualizada;
+
     }
 
     public void excluir(Long id){
-        Cidade cidade = cidadeRepositorio.findById(id).get();
+        Cidade cidade = cidadeRepositorio.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Cidade não encontrado."));
         cidadeRepositorio.delete(cidade);
+    }
+
+    @Transactional
+    public void importarDadosCsv(String caminhoArquivoCsv) throws CsvValidationException, NumberFormatException{
+        try (CSVReader reader = new CSVReader(new FileReader(caminhoArquivoCsv))){
+            String[] linha;
+
+            reader.readNext();
+            while((linha = reader.readNext()) != null){
+                Cidade cidade = new Cidade();
+                Estado estado = estadoRepositorio.findById(Long.parseLong(linha[3])).get();
+                cidade.setNome(linha[1]);
+                cidade.setStatus(linha[2]);
+                cidade.setEstado(estado);
+                cidade.setMarcada(linha[4]);
+                cidade.setDataCriacao(new Date());
+                cidadeRepositorio.save(cidade);
+            }
+        } catch(IOException e){
+            // Tratar as exceções
+        }
     }
 }
--- Backend/src/main/java/com/castilho/backend/servico/EstadoServico.java
+++ Backend/src/main/java/com/castilho/backend/servico/EstadoServico.java
@@ -1,5 +1,8 @@
 package com.castilho.backend.servico;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -7,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.castilho.backend.entidade.Estado;
 import com.castilho.backend.repositorio.EstadoRepositorio;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class EstadoServico {
@@ -17,14 +24,41 @@ public class EstadoServico {
         return estadoRepositorio.findAll();
     }
 
-    public Estado salvar(Estado estado){
-        return estadoRepositorio.save(estado);
+    public Estado inserir(Estado estado){
+        estado.setDataCriacao(new Date());
+        Estado estadoNovo = estadoRepositorio.saveAndFlush(estado);
+        return estadoNovo;
+    }
+
+    public Estado atualizar(Estado estado){
+        estado.setDataAtualizacao(new Date());
+        Estado estadoAtualizado = estadoRepositorio.saveAndFlush(estado);
+        return estadoAtualizado;
     }
 
     public void excluir(Long id){
         Estado estado = estadoRepositorio.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Estado não encontrado."));
         estadoRepositorio.delete(estado);
+    }
+
+    @Transactional
+    public void importarDadosCsv(String caminhoArquivoCsv) throws CsvValidationException{
+        try (CSVReader reader = new CSVReader(new FileReader(caminhoArquivoCsv))){
+            String[] linha;
+            reader.readNext();
+
+            while((linha = reader.readNext()) != null){
+                Estado estado = new Estado();
+                estado.setNome(linha[1]);
+                estado.setSigla(linha[2]);
+                estado.setStatus(linha[3]);
+                estado.setDataCriacao(new Date());
+                estadoRepositorio.save(estado);
+            }
+        } catch (IOException e) {
+            // Tratar exceções
+        }
     }
 
 }
