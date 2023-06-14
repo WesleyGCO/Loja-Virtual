174379010e38260f5999e7be17b6bcc534644818
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-06-13 21:40:01 -0300

    Comentados itens não utilizados

--- Backend/src/main/java/com/castilho/backend/exception/ExceptionHandlerControle.java
+++ Backend/src/main/java/com/castilho/backend/exception/ExceptionHandlerControle.java
@@ -1,18 +1,18 @@
 package com.castilho.backend.exception;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.castilho.backend.exception.model.ErroValidacaoObjeto;
-import com.castilho.backend.exception.model.ErroValidacaoResposta;
+//import com.castilho.backend.exception.model.ErroValidacaoObjeto;
+//import com.castilho.backend.exception.model.ErroValidacaoResposta;
 
 @ControllerAdvice
 public class ExceptionHandlerControle {
