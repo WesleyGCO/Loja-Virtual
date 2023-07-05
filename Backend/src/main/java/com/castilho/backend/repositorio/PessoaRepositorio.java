51f5baee9fa4d7633c33510aed167dc4134f60c7
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-07-04 21:26:23 -0300

    Criado busca de pessoa pelo id da cidade

--- Backend/src/main/java/com/castilho/backend/repositorio/PessoaRepositorio.java
+++ Backend/src/main/java/com/castilho/backend/repositorio/PessoaRepositorio.java
@@ -1,5 +1,7 @@
 package com.castilho.backend.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.castilho.backend.entidade.Pessoa;
 
 @Repository
 public interface PessoaRepositorio extends JpaRepository<Pessoa, Long> {
+
+    public List<Pessoa> findByCidade_id(Long idCidade);
 }
